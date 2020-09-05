df924d591fbbe661f0fa1cd6401c133d426d84f1
Andres Mora <andres@Andress-MacBook-Pro.local>
2020-09-05 16:56:16 -0400

    1pTest

--- belief/src/App.js
+++ belief/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>helloooooooooooo</h1>
+      <h1>hola</h1>
     </div>
   );
 }
