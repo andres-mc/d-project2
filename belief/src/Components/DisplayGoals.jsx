5801609b2402e508283b9d5ed8d5f7c88228c6de
Andres Mora <andres@Andress-MacBook-Pro.local>
2020-09-10 15:22:12 -0400

    5p css on the way

--- belief/src/Components/DisplayGoals.jsx
+++ belief/src/Components/DisplayGoals.jsx
@@ -3,9 +3,14 @@ import React from 'react';
 const Goals = (props) => {
   const { goal, prior } = props.goal.fields;
   return (
-    <div>
-      <h4>{goal}</h4>
-      <h5>{prior}</h5>
+    <div >
+      <ul className="display-cont">
+        <li><b>{goal}</b></li>
+        <li>{prior}</li>
+      </ul>
+      
+      {/* <h4>{goal}</h4>
+      <h5>{prior}</h5> */}
     </div>
 
   )
